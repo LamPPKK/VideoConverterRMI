06d995146955665485a2038573a6ca381483179b
minhduc3004 <ducminhvu304@gmail.com>
2020-10-28 22:12:50 +0700

    Sửa lại hàm khởi tạo Server

--- VideoConverterRMI_Server/src/Model/Server.java
+++ VideoConverterRMI_Server/src/Model/Server.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package Model;
 
 import it.sauronsoftware.jave.AudioAttributes;
 import it.sauronsoftware.jave.Encoder;
@@ -29,24 +30,24 @@ public class Server extends UnicastRemoteObject implements Convert {
     private static ServerSocket server;
     private static Socket connection;
 
-    public Server() throws RemoteException {
-
+    public Server() throws RemoteException, IOException {
+        this.server = new ServerSocket(3004);
+        try {
+            Registry reg = LocateRegistry.createRegistry(9999);
+            reg.rebind("convert from file",this);
+            Registry reg2 = LocateRegistry.createRegistry(8888);
+            reg2.rebind("convert from link youtube",this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        while (true) {
+            this.connection = server.accept();
+            System.out.println("1 devices has connected!");
+        }
     }
 
     public static void main(String[] args) throws RemoteException, IOException {
-        ServerSocket server = new ServerSocket(3004);
-        try {
-            Registry reg = LocateRegistry.createRegistry(9999);
-            reg.rebind("convert from file", new Server());
-            Registry reg2 = LocateRegistry.createRegistry(8888);
-            reg2.rebind("convert from link youtube", new Server());
-        } catch (Exception e) {
-            System.out.println("Exception " + e);
-        }
-        while (true) {
-            connection = server.accept();
-            System.out.println("1 devices has connected!");
-        }
+        new Server();
     }
 
     @Override
@@ -66,23 +67,26 @@ public class Server extends UnicastRemoteObject implements Convert {
             EncodingAttributes specifications = new EncodingAttributes();
             specifications.setFormat("mp3");
             specifications.setAudioAttributes(audio);
-            
-            System.out.println(source.getAbsolutePath().toString());
 
-//            String folder = "C:\\Users\\Vu Minh Duc\\Documents\\NetBeansProjects\\LapTrinhMang\\VideoConverterServer\\Music";
-//            String fileName = source.getName();
+//            System.out.println(source.getAbsolutePath().toString());
+            String folder = "C:\\Users\\Vu Minh Duc\\Documents\\NetBeansProjects\\VideoConverterRMI\\VideoConverterRMI_Server\\Music";
+            String fileName = source.getName();
 //            System.out.println(fileName.substring(0, fileName.lastIndexOf(".")).toString());
-//            File target = new File(folder + "\\" + fileName.substring(0, fileName.lastIndexOf(".")) + ".mp3");
-//            System.out.println(folder + "\\" + fileName.substring(0, fileName.lastIndexOf(".")) + ".mp3");
+            File target = new File(folder + "\\" + fileName.substring(0, fileName.lastIndexOf(".")) + ".mp3");
+            System.out.println(folder + "\\" + fileName.substring(0, fileName.lastIndexOf(".")) + ".mp3");
 //            System.out.println(target.getAbsolutePath());
-//            forMusic.encode(source, target, specifications);
-//            System.out.println("Done!");
-//            byte[] content = Files.readAllBytes(Paths.get(target.getPath()));
-//            DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
-//            dos.writeInt(content.length);
-//            dos.write(content);
-//            System.out.println("Sent!");
-//            dos.close();
+            forMusic.encode(source, target, specifications);
+            System.out.println("Done!");
+
+            byte[] content = Files.readAllBytes(Paths.get(target.getPath()));
+            System.out.println(content.length);
+            DataOutputStream dos = new DataOutputStream(this.connection.getOutputStream());
+            dos.writeInt(content.length);
+            System.out.println("Length sent!");
+            dos.flush();
+            dos.write(content);
+            System.out.println("Content sent!");
+            dos.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -92,4 +96,5 @@ public class Server extends UnicastRemoteObject implements Convert {
     public void ConvertFromYT() throws RemoteException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
 }
